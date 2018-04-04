ce283c20f80498705d2eda75b72314439026f35d
Rolando Collantes <rolandocollantes@mail.weber.edu>
2018-04-04 01:27:48 -0600

    Returns 10 random recommendations and displays them.

--- cgi-bin/engine.cgi
+++ cgi-bin/engine.cgi
@@ -5,21 +5,35 @@ import os, sys
 import json
 import pandas
 
-print("Content-Type: application/json", end="\r\n\r\n")
+def posterPath(imdbId):
+    imdbId = imdbId.zfill(7)
+    f2 = imdbId[:2]
+    n2 = imdbId[2:4]
+    path = "/".join(["posters", f2, n2, imdbId])
+    path += ".jpg"
+    return path
+
+imdbLink = lambda imdbId: "https://www.imdb.com/title/tt" + imdbId.zfill(7) + "/" 
+
+print("Content-Type: application/json; charset=utf-8", end="\r\n\r\n")
 
 # form = cgi.FieldStorage()
 # action = form.getvalue("random")
-movies = pandas.read_csv("movies.csv.gz", compression="gzip")
+try:
+    movies = pandas.read_pickle("movies.df.gz")
+except:
+    movies = pandas.read_csv("movies.csv.gz", compression="gzip")
+    movies.to_pickle("movies.df.gz")
+
 randoms = movies.sample(10)
 
 output = []
 for index, row in randoms.iterrows():
-    posterPath = str(row['imdbId']).zfill(7)
-    posterPath = posterPath[:2] + "/" + posterPath[2:4] + "/" + posterPath + ".jpg"
     o = {
         'id': index,
         'title': row['title'],
-        'poster': posterPath
+        'poster': posterPath(str(row['imdbId'])),
+        'link': imdbLink(str(row['imdbId']))
         }
     output.append(o)
 
--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 "use strict";
 
-const generateMovie = (posterURL, rating, movieId, removeLink) =>{
+const generateMovie = (title, posterURL, rating, movieId, removeLink, posterClickURL) =>{
     let posterDiv = document.createElement("div");
     posterDiv.classList.add("movie");
     posterDiv.dataset.movieId = movieId;
 
     // Image
-    let posterIMG = new Image();
+    let posterIMG = document.createElement("img");
     posterIMG.src = posterURL;
+    if(posterClickURL && posterClickURL.startsWith("https://")){
+        let link = document.createElement("a");
+        link.href = posterClickURL;
+        link.target = "_blank";
+        link.appendChild(posterIMG);
+        posterIMG = link;
+    }
     posterDiv.appendChild(posterIMG);
 
+    // Title
+    posterDiv.appendChild(document.createTextNode(title));
+    posterDiv.appendChild(document.createElement("br"));
+
     // Rating
-    posterDiv.appendChild(document.createTextNode(rating));
+    posterDiv.appendChild(document.createTextNode("Rating: " + rating));
 
     if(removeLink === true){
         let link = document.createElement("a");
-        link.addEventListener("click", e=>{removeMovie(movieId)})
+        link.href = "#";
+        link.addEventListener("click", e=>{removeMovie(movieId); e.preventDefault(); return false;})
         link.appendChild(document.createTextNode("Remove"));
         posterDiv.appendChild(document.createElement("br"));
         posterDiv.appendChild(link);
@@ -35,18 +47,51 @@ const removeMovie = movieId => {
     }
 }
 
+const replaceRecommendations = ()=>{
+    if(window.recs){
+        let header = document.querySelector("#recommendations > h3");
+        header.textContent = "Your recommendations:";
+        let div = document.querySelector("#recommendations > .manyMovies");
+        // Remove everything in it.
+        while(div.lastChild){
+            div.removeChild(div.lastChild);
+        }
+        window.recs.forEach(r => {
+            let movie = generateMovie(r.title, r.poster, "3/3", r.id, false, r.link);
+            div.appendChild(movie);
+        });
+    }
+}
+
 // Debouncing: it only runs 500ms after the last time this was called.
 // Repeated calls are ignored.
-let populateSuggestions = (e)=>{
+const  populateSuggestions = (e)=>{
     if(window.dtimer){
         clearInterval(window.dtimer);
         window.dtimer = null;
     }
     window.dtimer = setTimeout(()=>{
-        // True Function body here:
+        
+        // Get search suggestions here
+        if(e.target.value !== ""){
+            document.querySelector("#searchSuggestions").style.display = "block";
+        } else {
+            document.querySelector("#searchSuggestions").style.display = "none";
+            return;
+        }
 
         console.log("would search:", e.target.value);
     }, 500);
+}
+
+const  getInitialRecommendations = ()=>{
+    fetch("/cgi-bin/engine.cgi")
+    .then(res=>res.ok ? res.json() : Promise.reject())
+    .then(res=>{
+        window.recs = res;
+        replaceRecommendations();
+    })
+    .catch(res=>console.log("error: ",res))
 }
 
 document.querySelector("#generateMovie").addEventListener("click", e=>{
@@ -54,8 +99,10 @@ document.querySelector("#generateMovie").addEventListener("click", e=>{
     let rating = Math.random() * 4 + 1;
     rating = rating.toFixed(2);
     rating = rating + "/5";
-    let movie = generateMovie("", rating, movieId, true);
+    let movie = generateMovie("namehere", "", rating, movieId, true, false);
     document.querySelector("#selections").appendChild(movie);
 })
 
 document.querySelector("#searchBox").addEventListener("input", populateSuggestions);
+
+getInitialRecommendations();
